899a62a761371d92481f7f2627b5fccae1c20afd
zfw <zfw1226@gmail.com>
2017-05-31 19:20:49 +0800

    clean command

--- gym_unrealcv/envs/unrealcv_cmd.py
+++ gym_unrealcv/envs/unrealcv_cmd.py
@@ -32,12 +32,6 @@ class UnrealCv:
                  pitch = 0,
         )
 
-        self.arm_pose = dict(
-            base = 0,
-            upper = 0,
-            lower = 0
-        )
-
         self.init_unrealcv()
         self.pitch = 0 #-30
         self.color_dict = dict()
@@ -76,13 +70,13 @@ class UnrealCv:
     def read_image(self,cam_id , viewmode, show=False):
             # cam_id:0 1 2 ...
             # viewmode:lit,  =normal, depth, object_mask
-            readimage = 'vget /camera/{cam_id}/{viewmode}'
+            cmd = 'vget /camera/{cam_id}/{viewmode}'
 
             if self.docker:
-                img_dirs_docker = client.request(readimage.format(cam_id=cam_id, viewmode=viewmode))
+                img_dirs_docker = client.request(cmd.format(cam_id=cam_id, viewmode=viewmode))
                 img_dirs = self.envdir + img_dirs_docker[7:]
             else :
-                img_dirs = client.request(readimage.format(cam_id=cam_id, viewmode=viewmode))
+                img_dirs = client.request(cmd.format(cam_id=cam_id, viewmode=viewmode))
 
             image = cv2.imread(img_dirs)
             if show is True:
@@ -90,54 +84,36 @@ class UnrealCv:
             os.remove(img_dirs)
             return image
 
-    def set_arm_pose(self,controller_name,base,upper,lower):
-        setpose = 'vexec {controller_name} SetRotation {base} {upper} {lower}'
-        OK = client.request(setpose.format(controller_name=controller_name,
-                                            base=base, upper=upper, lower=lower))
-        self.arm_pose['base'] = base
-        self.arm_pose['upper'] = upper
-        self.arm_pose['lower'] = lower
-        return OK
 
-    def move_arm(self,controller_name,delt_base,delt_upper,delt_lower):
-        OK = self.set_arm_pose(
-            controller_name,
-            self.arm_pose['base'] + delt_base ,
-            self.arm_pose['upper'] + delt_upper,
-            self.arm_pose['lower'] + delt_lower
-        )
-        return OK
 
     def set_position(self,cam_id, x, y, z):
-        setposition = 'vset /camera/{cam_id}/location {x} {y} {z}'
-        client.request(setposition.format(cam_id=cam_id, x=x, y=y, z=z))
+        cmd = 'vset /camera/{cam_id}/location {x} {y} {z}'
+        client.request(cmd.format(cam_id=cam_id, x=x, y=y, z=z))
         self.cam['position'] = [x,y,z]
 
 
     def set_rotation(self,cam_id, roll, yaw, pitch):
-        rotation = 'vset /camera/{cam_id}/rotation {pitch} {yaw} {roll}'
-        client.request(rotation.format(cam_id=cam_id, pitch=pitch, yaw=yaw, roll=roll))
+        cmd = 'vset /camera/{cam_id}/rotation {pitch} {yaw} {roll}'
+        client.request(cmd.format(cam_id=cam_id, pitch=pitch, yaw=yaw, roll=roll))
         self.cam['rotation'] = (roll,yaw,pitch)
-        #self.yaw_now = yaw
 
     def get_position(self,cam_id):
-        getpos = 'vget /camera/{cam_id}/location'
-        pos = client.request(getpos.format(cam_id=cam_id))
+        cmd = 'vget /camera/{cam_id}/location'
+        pos = client.request(cmd.format(cam_id=cam_id))
         x,y,z = pos.split()
         self.cam['position'] = [float(x), float(y), float(z)]
         return self.cam['position']
 
-
     def get_rotation(self,cam_id):
-        getrotation = 'vget /camera/{cam_id}/rotation'
-        ori = client.request(getrotation.format(cam_id=cam_id))
+        cmd = 'vget /camera/{cam_id}/rotation'
+        ori = client.request(cmd.format(cam_id=cam_id))
         pitch,yaw,roll = ori.split()
         self.cam['rotation'] = (float(roll), float(yaw), float(pitch))
         return self.cam['rotation']
 
     def moveto(self,cam_id, x, y, z):
-        moveto = 'vset /camera/{cam_id}/moveto {x} {y} {z}'
-        client.request(moveto.format(cam_id=cam_id, x=x, y=y, z=z))
+        cmd = 'vset /camera/{cam_id}/moveto {x} {y} {z}'
+        client.request(cmd.format(cam_id=cam_id, x=x, y=y, z=z))
 
     def move(self,cam_id, angle, length):
 
@@ -153,7 +129,6 @@ class UnrealCv:
         if angle != 0 :
             self.set_rotation(cam_id, 0, yaw_exp, self.pitch)
 
-        # need subscribe the collision msg to skip the following
         self.cam['position'] = self.get_position(cam_id)
         error = self.error_position(self.cam['position'],(x_exp,y_exp,z_exp))
 
@@ -187,7 +162,6 @@ class UnrealCv:
 
     def get_mask(self,object_mask,object):
         r,g,b = self.color_dict[object]
-        #r,g,b =self.target_color
         lower_range = np.array([b-5,g-5,r-5])
         upper_range = np.array([b+5,g+5,r+5])
         mask = cv2.inRange(object_mask, lower_range, upper_range)
@@ -201,13 +175,9 @@ class UnrealCv:
 
     def get_bbox(self,object_mask,object):
         #only get an object's bounding box
-
         width = object_mask.shape[1]
         height = object_mask.shape[0]
-
         mask = self.get_mask(object_mask,object)
-
-
         nparray = np.array([[[0, 0]]])
         pixelpointsCV2 = cv2.findNonZero(mask)
 
@@ -231,42 +201,6 @@ class UnrealCv:
             boxes.append(box)
         return  boxes
 
-
-    def get_bbox_binary(self,object_mask):
-        #only get an object's bounding box
-
-        width = object_mask.shape[1]
-        height = object_mask.shape[0]
-
-        mask = self.get_mask_binary(object_mask)
-        cv2.imshow('mask',mask)
-        cv2.waitKey(10)
-        nparray = np.array([[[0, 0]]])
-        pixelpointsCV2 = cv2.findNonZero(mask)
-
-        if type(pixelpointsCV2) == type(nparray):# exist target in image
-            x_min = pixelpointsCV2[:,:,0].min()
-            x_max = pixelpointsCV2[:,:,0].max()
-            y_min = pixelpointsCV2[:,:,1].min()
-            y_max = pixelpointsCV2[:,:,1].max()
-            #print x_min, x_max ,y_min, y_max
-            box = ((x_min/float(width),y_min/float(height)),#left top
-                   (x_max/float(width),y_max/float(height)))#right down
-        else:
-            box = ((0,0),(0,0))
-
-        return mask , box
-
-
-    def build_color_dic(self):
-        objects = self.get_objects()
-        objects = objects.split()
-        object_dict = dict()
-        for obj in objects:
-            color = self.get_object_color(obj)
-            object_dict[obj] = color
-        return object_dict
-
     def target_color_dic(self,objects):
         object_dict = dict()
         for obj in objects:
@@ -274,17 +208,15 @@ class UnrealCv:
             object_dict[obj] = color
         return object_dict
 
-
     def get_object_pos(self,object):
         pos = client.request('vget /object/{obj}/location'.format(obj = object))
         x,y,z = pos.split()
         return (float(x),float(y),float(z))
+
     def get_objects_pos(self,objects):
         pos = []
         for obj in objects:
-            p = client.request('vget /object/{obj}/location'.format(obj=obj))
-            x, y, z = p.split()
-            pos.append((float(x),float(y),float(z)))
+            pos.append(self.get_object_pos(obj))
         return pos
 
     def get_pos(self):
