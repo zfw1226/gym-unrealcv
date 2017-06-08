11105b5f2a6e22d98a35f0f29012b259548b2839
zfw <zfw1226@gmail.com>
2017-06-08 22:35:54 +0800

    add docker dependency
    Signed-off-by: zfw <zfw1226@gmail.com>

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_unrealcv',
       version='0.0.1',
-      install_requires=['gym','opencv-python',],  # And any other dependencies foo needs
+      install_requires=['gym','opencv-python','docker'],  # And any other dependencies foo needs
 )  
