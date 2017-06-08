3d60e4f831cc141f4534616b5ced64e67e0fe3eb
zfw <zfw1226@gmail.com>
2017-06-08 22:24:12 +0800

    modify wget bug
    Signed-off-by: zfw <zfw1226@gmail.com>

--- gym_unrealcv/envs/UnrealEnv/RealisticRendering.sh
+++ gym_unrealcv/envs/UnrealEnv/RealisticRendering.sh
@@ -1,3 +1,3 @@
-wegt https://s3-us-west-1.amazonaws.com/unreal-rl/RealisticRendering_RL.zip
+wget https://s3-us-west-1.amazonaws.com/unreal-rl/RealisticRendering_RL.zip
 unzip RealisticRendering_RL.zip
 rm RealisticRendering_RL.zip
