2aa2d346b9cfb921e810a432c1d4396015b06681
zfw <zfw1226@gmail.com>
2017-06-08 15:34:31 +0800

    auto loading env
    Signed-off-by: zfw <zfw1226@gmail.com>

---
+++ gym_unrealcv/envs/UnrealEnv/RealisticRendering.sh
@@ -0,0 +1,4 @@
+wegt https://s3-us-west-1.amazonaws.com/unreal-rl/RealisticRendering_RL.zip
+unzip RealisticRendering_RL.zip
+rm RealisticRendering_RL.zip
+mv RealisticRendering_RL UnrealEnv/RealisticRendering
