8c3089f5a91d1430a0942ec1a6c1dff5d846bc20
zfw <zfw1226@gmail.com>
2017-06-09 15:03:02 +0800

    prepare RealisticRendering binary
    Signed-off-by: zfw <zfw1226@gmail.com>

---
+++ RealisticRendering.sh
@@ -0,0 +1,4 @@
+wget https://s3-us-west-1.amazonaws.com/unreal-rl/RealisticRendering_RL.zip
+unzip RealisticRendering_RL.zip
+rm RealisticRendering_RL.zip
+mv RealisticRendering_RL gym_unrealcv/envs/UnrealEnv
