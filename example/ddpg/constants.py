bbb7fc60d41306e1f9764ff1647d6453a889f78f
zfw <zfw1226@gmail.com>
2017-06-10 16:50:42 +0800

    new ddpg
    Signed-off-by: zfw <zfw1226@gmail.com>

---
+++ example/ddpg/constants.py
@@ -0,0 +1,47 @@
+ENV_NAME = 'unrealcv-v0'
+
+CONTINUE = False
+RESTART_EP = 4000
+TRAIN = True
+
+SHOW = True
+MAP = False
+
+MAX_EPOCHS = 10000
+
+MAX_STEPS_PER_EPOCH = 100
+MEMORY_SIZE = 20000
+LEARN_START_STEP = 3000
+
+INPUT_SIZE = 200
+INPUT_CHANNELS = 3
+
+BATCH_SIZE = 64
+
+VELOCITY_MAX = 100
+ANGLE_MAX = 45
+
+LEARNINGRATE_CRITIC  = 0.001
+LEARNINGRATE_ACTOR = 0.0001
+TARGET_UPDATE_RATE = 0.001
+
+
+GAMMA = 0.95
+INITIAL_EPSILON = 1  # starting value of epsilon
+FINAL_EPSILON = 0.1  # final value of epsilon
+
+MAX_EXPLORE_STEPS = 20000
+TEST_INTERVAL_EPOCHS = 100
+SAVE_INTERVAL_EPOCHS = 100
+
+
+MONITOR_DIR = 'log/monitor/' #the path to save monitor file
+MODEL_DIR = 'log/model' # the path to save deep model
+PARAM_DIR = 'log/param' # the path to save the parameters
+TRA_DIR = 'log/trajectory.csv' # the path to save trajectory
+
+#the path to reload weights, monitor and params
+critic_weights_path = 'log/model/ep' + str(RESTART_EP) + '/actormodel.h5'
+actor_weights_path = 'log/model/ep' + str(RESTART_EP) + '/criticmodel.h5'
+monitor_path = 'log/monitor/'+ str(RESTART_EP)
+params_json = 'log/param/' + str(RESTART_EP) + '.json'
---
+++ example/ddpg/memory.py
@@ -0,0 +1,59 @@
+import random
+import numpy as np
+
+class Memory:
+    """
+    This class provides an abstraction to store the [s, a, r, a'] elements of each iteration.
+    Instead of using tuples (as other implementations do), the information is stored in lists 
+    that get returned as another list of dictionaries with each key corresponding to either 
+    "state", "action", "reward", "nextState" or "isFinal".
+    """
+    def __init__(self, size):
+        self.size = size
+        self.currentPosition = 0
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.newStates = []
+        self.finals = []
+
+
+    def getCurrentSize(self) :
+        return len(self.states)
+
+    def getMemory(self, index): 
+        return {'state': self.states[index],'action': self.actions[index], 'reward': self.rewards[index], 'newState': self.newStates[index], 'isFinal': self.finals[index]}
+
+    def addMemory(self, state, action, reward, newState, isFinal) :
+        if (self.currentPosition >= self.size - 1) :
+            self.currentPosition = 0
+        if (len(self.states) > self.size) :
+            self.states[self.currentPosition] = state
+            self.actions[self.currentPosition] = action
+            self.rewards[self.currentPosition] = reward
+            self.newStates[self.currentPosition] = newState
+            self.finals[self.currentPosition] = isFinal
+        else :
+            self.states.append(state)
+            self.actions.append(action)
+            self.rewards.append(reward)
+            self.newStates.append(newState)
+            self.finals.append(isFinal)
+        
+        self.currentPosition += 1
+
+    def getMiniBatch(self, size) :
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        newState_batch = []
+        isFinal_batch = []
+        indices = random.sample(np.arange(len(self.states)), min(size,len(self.states)) )
+        for index in indices:
+            state_batch.append(self.states[index][0])
+            action_batch.append(self.actions[index])
+            reward_batch.append(self.rewards[index])
+            newState_batch.append(self.newStates[index][0])
+            isFinal_batch.append(self.finals[index])
+
+        return state_batch, action_batch, reward_batch, newState_batch, isFinal_batch
