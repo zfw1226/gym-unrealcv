427aef6e87d276e1441491c8b3936e35d4083da6
zfw <zfw1226@gmail.com>
2017-06-15 00:04:03 +0800

    baseline
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/dqn-dev/constants.py
+++ example/dqn-dev/constants.py
@@ -1,7 +1,7 @@
-ENV_NAME = 'Unrealcv-Search-v1'
+ENV_NAME = 'Unrealcv-Search-v3'
 
 CONTINUE = False #load a pre-trained model
-RESTART_EP = 9800 # the episode number of the pre-trained model
+RESTART_EP = 1800 # the episode number of the pre-trained model
 
 TRAIN = True # train the network
 USE_TARGET_NETWORK = False # use the target network
@@ -11,7 +11,7 @@ MAP = False # show the trajectory in 2d map
 MAX_EPOCHS = 100000 # max episode number
 
 MEMORY_SIZE = 10000
-LEARN_START_STEP = 100
+LEARN_START_STEP = 5000
 INPUT_SIZE = 150
 INPUT_CHANNELS = 3
 BATCH_SIZE = 64
@@ -23,12 +23,12 @@ MAX_EXPLORE_STEPS = 5000
 TEST_INTERVAL_EPOCHS = 100
 SAVE_INTERVAL_EPOCHS = 200
 
-MONITOR_DIR = 'log/monitor/' #the path to save monitor file
-MODEL_DIR = 'log/model' # the path to save deep model
-PARAM_DIR = 'log/param' # the path to save the parameters
-TRA_DIR = 'log/trajectory.csv' # the path to save trajectory
+MONITOR_DIR = 'log-basev3/monitor/' #the path to save monitor file
+MODEL_DIR = 'log-basev3/model' # the path to save deep model
+PARAM_DIR = 'log-basev3/param' # the path to save the parameters
+TRA_DIR = 'log-basev3/trajectory.csv' # the path to save trajectory
 
 #the path to reload weights, monitor and params
-weights_path = 'log-door/model/dqn_ep' + str(RESTART_EP)+ '.h5'
-monitor_path = 'log-door/monitor/'+ str(RESTART_EP)
-params_json = 'log-door/param/dqn_ep' + str(RESTART_EP) + '.json'
+weights_path = 'log-nodis/model/dqn_ep' + str(RESTART_EP)+ '.h5'
+monitor_path = 'log-nodis/monitor/'+ str(RESTART_EP)
+params_json = 'log-nodis/param/dqn_ep' + str(RESTART_EP) + '.json'
