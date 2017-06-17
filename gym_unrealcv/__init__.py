162ed0243725dec55942991c69d64d494ac3bae5
zfw <zfw1226@gmail.com>
2017-06-18 00:38:51 +0800

    new env
    Signed-off-by: zfw <zfw1226@gmail.com>

--- gym_unrealcv/__init__.py
+++ gym_unrealcv/__init__.py
@@ -10,15 +10,36 @@ register(
     entry_point='gym_unrealcv.envs:UnrealCvSearch'
 )
 
-# adding reward by distance
+
+# single target, reward by trigger
 register(
     id='Unrealcv-Search-v1',
-    entry_point='gym_unrealcv.envs:UnrealCvSearch_v1'
+    entry_point='gym_unrealcv.envs:UnrealCvSearch_v4',
+    kwargs = {'setting_file' : 'search_rr_plant7.yaml'}
 )
 
-# target driven and reward by distance
 register(
     id='Unrealcv-Search-v2',
-    entry_point='gym_unrealcv.envs:UnrealCvSearch_v2'
+    entry_point='gym_unrealcv.envs:UnrealCvSearch_v4',
+    kwargs = {'setting_file' : 'search_rr_plant8.yaml'}
+)
+
+register(
+    id='Unrealcv-Search-v3',
+    entry_point='gym_unrealcv.envs:UnrealCvSearch_v4',
+    kwargs = {'setting_file' : 'search_rr_door41.yaml'}
+)
+
+# multi targets, reward by trigger
+register(
+    id='Unrealcv-Search-v4',
+    entry_point='gym_unrealcv.envs:UnrealCvSearch_v4',
+    kwargs = {'setting_file' : 'search_rr_plant78.yaml'},
+)
+
+register(
+    id='Unrealcv-Search-v5',
+    entry_point='gym_unrealcv.envs:UnrealCvSearch_v4',
+    kwargs = {'setting_file' : 'search_arch1_door12.yaml'}
 )
 
