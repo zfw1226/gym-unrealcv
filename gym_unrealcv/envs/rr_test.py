029040a8e32226e4ce2fc53193e331f52f0ba525
zfw <zfw1226@gmail.com>
2017-06-18 18:14:00 +0800

    generate ground truth
    Signed-off-by: zfw <zfw1226@gmail.com>

---
+++ gym_unrealcv/envs/rr_test.py
@@ -0,0 +1,59 @@
+from unrealcv_cmd import  UnrealCv
+import time
+import run_docker
+import yaml
+# a toolkit for sampling groundtruth bounding box in environment
+
+
+TARGETS = [ 'SM_CoffeeTable_14', 'Couch_13','SM_Couch_1seat_5','Statue_48','SM_TV_5', 'SM_DeskLamp_5',
+ 'SM_Plant_7', 'SM_Plant_8', 'SM_Door_37', 'SM_Door_39', 'SM_Door_41']
+ENV_NAME = 'RealisticRendering'
+cam_id = 0
+
+
+#start docker
+docker = run_docker.RunDocker()
+env_ip, env_dir = docker.start(ENV_NAME=ENV_NAME)
+print env_ip
+print env_dir
+#connect unrealcv
+unrealcv = UnrealCv(cam_id, ip=env_ip, targets=TARGETS, env=env_dir)
+
+setting = dict()
+#TARGETS = unrealcv.get_objects()
+setting['targets'] = TARGETS
+setting['env_name'] = ENV_NAME
+setting['cam_id'] = cam_id
+setting['num'] = 5
+for i in range(setting['num']):
+    info =dict()
+    info['position'] = unrealcv.get_position(cam_id)
+    info['rotation'] = unrealcv.get_rotation(cam_id)
+    object_mask = unrealcv.read_image(cam_id, 'object_mask')
+    info['width'] = object_mask.shape[0]
+    info['height'] = object_mask.shape[1]
+    bboxes = unrealcv.get_bboxes_obj(object_mask, TARGETS)
+    for (obj,box) in bboxes.items():
+        (xmin, ymin), (xmax, ymax) = box
+        boxarea = (ymax - ymin) * (xmax - xmin)
+        if boxarea > 0:
+            box = dict()
+            box['bbox'] = [float(xmin),float(ymin),float(xmax),float(ymax)]
+            box['area'] = float(boxarea)
+            info[obj] = box
+    print info
+    setting[i] = info
+    lit = unrealcv.read_image(cam_id, 'lit')
+    time.sleep(10)
+
+print setting
+f = open('rr_bbox.yaml','w')
+f.write(yaml.dump(setting))
+f.close()
+
+docker.close()
+
+def save_setting(dict,filename):
+    f = open(filename,'w')
+    f.write(yaml.dump(dict))
+    f.close()
