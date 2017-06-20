05742931a0cd4b4d40cf9857528fed17d2b5fbac
zfw <zfw1226@gmail.com>
2017-06-20 15:21:55 +0800

    estimate continues direction
    Signed-off-by: zfw <zfw1226@gmail.com>

---
+++ example/dqn-linear/io_util.py
@@ -0,0 +1,178 @@
+
+import csv
+import os
+import cv2
+import keras.backend as K
+from constants import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+def detect_monitor_files(training_dir):
+    return [os.path.join(training_dir, f) for f in os.listdir(training_dir) if f.startswith('openaigym')]
+
+def clear_monitor_files(training_dir):
+    if not os.path.exists(training_dir):
+        os.makedirs(training_dir)
+    files = detect_monitor_files(training_dir)
+    if len(files) == 0:
+        return
+    for file in files:
+        os.unlink(file)
+
+def show_info( info, cv_img, direction):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    #cv2.putText(cv_img, 'Reward:' + str(round(info['Reward'],3)), (230, 450), font, 0.5, (255, 255, 255), 2)
+    #cv2.putText(cv_img, 'Direction', (220, 450), font, 0.5, (255, 255, 255), 2)
+    cv2.putText(cv_img, 'Action', (330, 450), font, 0.5, (255, 255, 255), 2)
+    cv2.putText(cv_img, 'Reward:' + str(round(info['Reward'], 3)), (220, 450), font, 0.5, (255, 255, 255), 2)
+    #cv2.putText(cv_img, 'Velocity:' + str(info['Action'][0]), (500, 430), font, 0.5, (255, 255, 255), 2)
+    cv2.putText(cv_img, 'Dir_pre:' + str(direction), (500, 450), font, 0.5, (255, 255, 255), 2)
+    cv2.putText(cv_img, 'Dir_truth:' + str(((info['Direction']+180)%360)/360.0), (500, 430), font, 0.5, (255, 255, 255), 2)
+    #cv2.putText(cv_img, 'Ang_pre:' + str(angle) , (200, 430), font, 0.5, (255, 255, 255), 2)
+
+
+    # 0  back
+    # 1 forward -30 ~30
+    # 2 right 30 ~90
+    # 3 left  -30 ~ -90
+    '''color = [(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255)]
+    color[angle] = (0, 255, 0)
+    center = 250
+    cv2.circle(cv_img, (center, 400), 8, color[1], -1) #forward
+    cv2.circle(cv_img, (center+20, 420), 8, color[2], -1) #right
+    cv2.circle(cv_img, (center-20, 420), 8, color[3], -1) #left
+    cv2.circle(cv_img, (center, 430), 8, color[0], -1) #back'''
+
+
+    #action
+    action_x = 350
+    action_y = 440
+    color = [(255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255)]
+    if info['Action'][0] == 30:
+        color[0] = (0, 0, 255)
+    elif info['Action'][0] == 20 and info['Action'][1] == 15:
+        color[1] = (0, 0, 255)
+    elif info['Action'][0] == 20 and info['Action'][1] == -15:
+        color[2] = (0, 0, 255)
+    elif info['Action'][0] == 10 and info['Action'][1] == 30:
+        color[3] = (0, 0, 255)
+    elif info['Action'][0] == 10 and info['Action'][1] == -30:
+        color[4] = (0, 0, 255)
+
+    cv2.circle(cv_img, (action_x, action_y - 50), 8, color[0], -1) #forward
+    cv2.circle(cv_img, (action_x+10, action_y - 30), 8, color[1], -1) #right
+    cv2.circle(cv_img, (action_x-10, action_y - 30), 8, color[2], -1) #left
+    cv2.circle(cv_img, (action_x+20, action_y - 10), 8, color[3], -1)  # right
+    cv2.circle(cv_img, (action_x-20, action_y - 10), 8, color[4], -1)  # left
+
+
+    if info['Trigger']:
+        cv2.putText(cv_img, 'Trigger', (420, 450), font, 0.5, (0, 0, 255), 2)
+        cv2.circle(cv_img, (440, 420), 15, (0, 0, 255), -1)
+    else:
+        cv2.putText(cv_img, 'Trigger', (420, 450), font, 0.5, (255, 255, 255), 2)
+        cv2.circle(cv_img, (440, 420), 15, (255, 255, 255), -1)
+
+    if info['Collision']:
+        cv2.circle(cv_img, (120, 420), 15, (0, 0, 255), -1)
+        cv2.putText(cv_img, 'Collision', (100, 450), font, 0.5, (0, 0, 255), 2)
+    else:
+        cv2.circle(cv_img, (120, 420), 15, (0, 255, 0), -1)
+        cv2.putText(cv_img, 'Collision', (100, 450), font, 0.5, (0, 255, 0), 2)
+
+    for box in info['Bbox']:
+            xmin = int(box[0][0] * cv_img.shape[1])
+            xmax = int(box[1][0] * cv_img.shape[1])
+            ymin = int(box[0][1] * cv_img.shape[0])
+            ymax = int(box[1][1] * cv_img.shape[0])
+            cv2.rectangle(cv_img, (xmin, ymin), (xmax, ymax), (50, 255, 50))
+
+    cv2.imshow('info_show', cv_img)
+    cv2.waitKey(3)
+
+def save_trajectory(info,filedir,epoch):#save when every step finished
+    trajec = dict()
+    with open(filedir, 'a') as f:
+        header = ['epoch', 'step', 'x', 'y', 'z', 'yaw','reward','collision','done',]
+        f_csv = csv.DictWriter(f, header)
+    # list to dic and append dic to list
+        trajec['x'],trajec['y'],trajec['z'],trajec['yaw'] = info['Trajectory'][-1]
+        trajec['epoch'] = epoch
+        trajec['step'] = info['Steps']
+        trajec['reward'] = info['Reward']
+        trajec['collision'] = info['Collision']
+        trajec['done'] = info['Done']
+        f_csv.writerows([trajec])
+
+def create_csv_header(DIR):
+    with open(DIR, 'a') as f:
+        header = ['epoch', 'step', 'x', 'y', 'z', 'yaw','reward','collision','done']
+        f_csv = csv.DictWriter(f, header)
+        f_csv.writeheader()
+
+def preprocess_img(image):
+    cv_image = cv2.resize(image, (INPUT_SIZE, INPUT_SIZE))
+    img_processed = cv_image.reshape(1, INPUT_CHANNELS, INPUT_SIZE, INPUT_SIZE)
+    img_processed = img_processed / 255.0
+    if K.image_dim_ordering() == 'tf':
+        img_processed = img_processed.transpose(0, 2, 3, 1)
+    return img_processed
+
+def live_plot(info):
+    plt.ion()
+    X = []
+    Y = []
+    global line
+    plt.xlim((-200, 200))
+    plt.ylim((-550, 350))
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.scatter(info['Target'][0], -info['Target'][1], c='red', s=150,  alpha=0.6, edgecolors='white',label='target object')
+    #print info['Trajectory']
+    if len(info['Trajectory']) > 0:
+        for pos in info['Trajectory']:
+            #print pos
+            X.append(pos[0])
+            Y.append(-pos[1])
+
+        if info['Steps'] == 0:
+            line = plt.plot(X, Y, 'b')
+            plt.scatter(X[0], Y[0], c='blue', s=50, alpha=0.6, edgecolors='white', label='current path')  # plot start point
+
+        if info['Done']:
+            line[-1].set_xdata(X)
+            line[-1].set_ydata(Y)
+            line[-1].set_linestyle('--')
+
+            if info['Reward'] > 0:
+                line[-1].set_color('r')
+                plt.scatter(X[-1], Y[-1], c='green', s=info['Reward']*20, alpha=0.6, edgecolors='white')# right trigger
+            elif info['Collision']:
+                line[-1].set_color('k')
+                plt.scatter(X[-1], Y[-1], c='magenta', s=20, alpha=0.6, edgecolors='white')
+
+        else:
+            line[-1].set_xdata(X)
+            line[-1].set_ydata(Y)
+            line[-1].set_color('b')
+
+        plt.pause(0.01)
+
+def onehot(num,len):
+    onehot = np.zeros(len)
+    onehot[num] = 1
+    return onehot
+
+def onehot_angle(angle,len):
+    onehot = np.zeros(len)
+    if angle > 90 and angle < 270: # back
+        angle_id = 0
+    elif (angle+30)%360 < 60: # forward -30 ~30
+        angle_id = 1
+    elif angle < 90: # right 30 ~90
+        angle_id = 2
+    elif angle > 270: # left  -30 ~ -90
+        angle_id = 3
+
+    onehot[angle_id] = 1
+    return onehot,angle_id
---
+++ example/dqn-linear/run.py
@@ -0,0 +1,170 @@
+import gym
+import gym_unrealcv
+import time
+from distutils.dir_util import copy_tree
+import os
+import json
+import random
+import numpy as np
+import dqn
+import cv2
+from constants import *
+import io_util
+from gym import wrappers
+import csv
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    env = gym.make(ENV_NAME)
+
+    ACTION_SIZE = len(ACTION_LIST)
+    ANGLE_SIZE = 4
+
+    #init log file
+    if not os.path.exists(MODEL_DIR):
+        os.makedirs(MODEL_DIR)
+    if not os.path.exists(PARAM_DIR):
+        os.makedirs(PARAM_DIR)
+
+
+    #load init param
+    if not CONTINUE:
+        explorationRate = INITIAL_EPSILON
+        current_epoch = 0
+        stepCounter = 0
+        loadsim_seconds = 0
+        Agent = dqn.DeepQ(ACTION_SIZE, MEMORY_SIZE, GAMMA, LEARNING_RATE,
+                          INPUT_SIZE, INPUT_SIZE,INPUT_CHANNELS,USE_TARGET_NETWORK,ANGLE_SIZE)
+        env = wrappers.Monitor(env, MONITOR_DIR + 'tmp', write_upon_reset=True,force=True)
+
+        #io_util.create_csv_header(TRA_DIR)
+
+    else:
+        #Load weights, monitor info and parameter info.
+        with open(params_json) as outfile:
+            d = json.load(outfile)
+            explorationRate = d.get('explorationRate')
+            current_epoch = d.get('current_epoch')
+            stepCounter = d.get('stepCounter')
+            loadsim_seconds = d.get('loadsim_seconds')
+            Agent = dqn.DeepQ(
+                ACTION_SIZE,
+                MEMORY_SIZE,
+                GAMMA,
+                LEARNING_RATE,
+                INPUT_SIZE,
+                INPUT_SIZE,
+                INPUT_CHANNELS,
+                USE_TARGET_NETWORK,
+                ANGLE_SIZE
+            )
+            Agent.loadWeights(weights_path)
+            io_util.clear_monitor_files(MONITOR_DIR + 'tmp')
+            copy_tree(monitor_path, MONITOR_DIR + 'tmp')
+            env = wrappers.Monitor(env, MONITOR_DIR + 'tmp', write_upon_reset=True,resume=True)
+
+        #io_util.create_csv_header(TRA_DIR)
+    if not os.path.exists(TRA_DIR):
+        io_util.create_csv_header(TRA_DIR)
+
+    dir_loss = []
+    #main loop
+    try:
+        start_time = time.time()
+        for epoch in xrange(current_epoch, MAX_EPOCHS, 1):
+            obs = env.reset()
+            observation = io_util.preprocess_img(obs)
+            cumulated_reward = 0
+            direction_linear = 0
+
+            if ((epoch) % TEST_INTERVAL_EPOCHS != 0 or stepCounter < LEARN_START_STEP) and TRAIN is True :  # explore
+                EXPLORE = True
+            else:
+                EXPLORE = False
+                print ("Evaluate Model")
+            for t in xrange(1000):
+
+                start_req = time.time()
+
+                if EXPLORE is True: #explore
+                    [action,direction_pre] = Agent.feedforward(observation, explorationRate)
+
+                    dir_loss.append(abs(direction_pre - direction_linear))
+                    dir_loss_mean = np.array(dir_loss[-min(100,len(dir_loss)):]).mean()
+
+                    obs_new, reward, done, info = env.step(ACTION_LIST[action])
+                    newObservation = io_util.preprocess_img(obs_new)
+                    stepCounter += 1
+
+                    #angle_onehot, angle_id = io_util.onehot_angle(info['Direction'],ANGLE_SIZE)
+
+                    direction_linear = ((info['Direction']+180)%360)/360.0
+
+                    Agent.addMemory_new(observation, action, reward, newObservation, done, direction_linear )
+                    observation = newObservation
+                    if stepCounter == LEARN_START_STEP:
+                        print("Starting learning")
+
+                    if Agent.getMemorySize() >= LEARN_START_STEP:
+                        Agent.learnOnMiniBatch(BATCH_SIZE)
+
+                        if explorationRate > FINAL_EPSILON and stepCounter > LEARN_START_STEP:
+                            explorationRate -= (INITIAL_EPSILON - FINAL_EPSILON) / MAX_EXPLORE_STEPS
+                        #elif stepCounter%(MAX_EXPLORE_STEPS * 1.5) == 0 :
+                            #explorationRate = 0.99
+                            #print 'Reset Exploration Rate'
+
+                #test
+                else:
+                    [action, direction_pre] = Agent.feedforward(observation,0)
+
+                    obs_new, reward, done, info = env.step(ACTION_LIST[action])
+                    newObservation = io_util.preprocess_img(obs_new)
+                    observation = newObservation
+
+                if SHOW:
+                    io_util.show_info(info, obs_new, direction_pre)
+                if MAP:
+                    io_util.live_plot(info)
+
+                io_util.save_trajectory(info,TRA_DIR,epoch)
+
+                cumulated_reward += reward
+                if done:
+                    m, s = divmod(int(time.time() - start_time + loadsim_seconds), 60)
+                    h, m = divmod(m, 60)
+
+                    print ("EP " + str(epoch) +" Csteps= " + str(stepCounter) + " - {} steps".format(t + 1) + " - CReward: " + str(
+                        round(cumulated_reward, 2)) + "  Eps=" + str(round(explorationRate, 2)) + "  Time: %d:%02d:%02d" % (h, m, s) + " Direction Loss: " + str(dir_loss_mean) )
+                        # SAVE SIMULATION DATA
+                    if (epoch) % SAVE_INTERVAL_EPOCHS == 0 and TRAIN is True:
+                        # save model weights and monitoring data
+                        print 'Save model'
+                        Agent.saveModel(MODEL_DIR + '/dqn_ep' + str(epoch) + '.h5')
+
+                        #backup monitor file
+                        copy_tree(MONITOR_DIR+ 'tmp', MONITOR_DIR + str(epoch))
+
+                        parameter_keys = ['explorationRate', 'current_epoch','stepCounter', 'FINAL_EPSILON','loadsim_seconds']
+                        parameter_values = [explorationRate, epoch, stepCounter,FINAL_EPSILON, int(time.time() - start_time + loadsim_seconds)]
+                        parameter_dictionary = dict(zip(parameter_keys, parameter_values))
+                        with open(PARAM_DIR + '/dqn_ep' + str(epoch) + '.json','w') as outfile:
+                            json.dump(parameter_dictionary, outfile)
+
+
+
+                    break
+
+
+
+    except KeyboardInterrupt:
+        print("Shutting down")
+        #cv2.destroyAllWindows()
+        #env.monitor.close() # not needed in latest gym update
+        #env.close_docker()
+        env.close()
