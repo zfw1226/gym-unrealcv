e2139216855314473cb283d2c2ae19866f68d47d
zfw <zfw1226@gmail.com>
2017-06-26 17:31:11 +0800

    add log_name_save
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/ddpg/constants.py
+++ example/ddpg/constants.py
@@ -1,4 +1,4 @@
-ENV_NAME = 'Unrealcv-Search-v2'
+ENV_NAME = 'Search-RrPlant-v1'
 
 CONTINUE = False
 RESTART_EP = 8000
@@ -7,10 +7,10 @@ SHOW = True
 MAP = False
 
 MAX_EPOCHS = 100000
-MAX_STEPS_PER_EPOCH = 1000
-MEMORY_SIZE = 20000
+MAX_STEPS_PER_EPOCH = 10000
+MEMORY_SIZE = 10000
 LEARN_START_STEP = 5000
-INPUT_SIZE = 100
+INPUT_SIZE = 150 # pre 100
 INPUT_CHANNELS = 3
 BATCH_SIZE = 64
 VELOCITY_MAX = 100
@@ -28,14 +28,15 @@ MAX_EXPLORE_STEPS = 10000
 TEST_INTERVAL_EPOCHS = 1000
 SAVE_INTERVAL_EPOCHS = 200
 
-
-MONITOR_DIR = 'log/monitor/' #the path to save monitor file
-MODEL_DIR = 'log/model' # the path to save deep model
-PARAM_DIR = 'log/param' # the path to save the parameters
-TRA_DIR = 'log/trajectory.csv' # the path to save trajectory
+LOG_NAME_SAVE = 'log-baseline-v2.2'
+MONITOR_DIR = LOG_NAME_SAVE + '/monitor/' #the path to save monitor file
+MODEL_DIR = LOG_NAME_SAVE + '/model' # the path to save deep model
+PARAM_DIR = LOG_NAME_SAVE + '/param' # the path to save the parameters
+TRA_DIR = LOG_NAME_SAVE + '/trajectory.csv' # the path to save trajectory
 
 #the path to reload weights, monitor and params
-critic_weights_path = 'log6.11/model/ep' + str(RESTART_EP) + 'Critic_model.h5'
-actor_weights_path = 'log6.11/model/ep' + str(RESTART_EP) + 'Actor_model.h5'
-monitor_path = 'log6.11/monitor/'+ str(RESTART_EP)
-params_json = 'log6.11/param/' + str(RESTART_EP) + '.json'
+LOG_NAME_READ = 'log-angle-v4.0'
+critic_weights_path = LOG_NAME_READ + '/model/ep' + str(RESTART_EP) + 'Critic_model.h5'
+actor_weights_path = LOG_NAME_READ + '/model/ep' + str(RESTART_EP) + 'Actor_model.h5'
+monitor_path = LOG_NAME_READ + '/monitor/'+ str(RESTART_EP)
+params_json = LOG_NAME_READ + '/param/' + str(RESTART_EP) + '.json'
