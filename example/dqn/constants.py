693b8087ccc34f4a07aa7e0cc15dff49bc38ab9e
zfw <zfw1226@gmail.com>
2017-06-26 17:38:26 +0800

    ADD lognamesave
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/dqn/constants.py
+++ example/dqn/constants.py
@@ -1,34 +1,45 @@
-ENV_NAME = 'Unrealcv-Search-v0'
+ENV_NAME = 'Unrealcv-Search-v4'
 
-CONTINUE = False #load a pre-trained model
-RESTART_EP = 3600 # the episode number of the pre-trained model
+CONTINUE = True #load a pre-trained model
+RESTART_EP = 6000 # the episode number of the pre-trained model
 
-TRAIN = True # train the network
-USE_TARGET_NETWORK = True # use the target network
+TRAIN = False # train the network
+USE_TARGET_NETWORK = False # use the target network
 SHOW = True # show the current state, reward and action
 MAP = False # show the trajectory in 2d map
 
 MAX_EPOCHS = 10000 # max episode number
-
-MEMORY_SIZE = 20000
+MEMORY_SIZE = 10000
 LEARN_START_STEP = 5000
 INPUT_SIZE = 150
 INPUT_CHANNELS = 3
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 LEARNING_RATE = 1e-3  # 1e6
 GAMMA = 0.95
 INITIAL_EPSILON = 1  # starting value of epsilon
 FINAL_EPSILON = 0.1  # final value of epsilon
-MAX_EXPLORE_STEPS = 10000
-TEST_INTERVAL_EPOCHS = 100
+MAX_EXPLORE_STEPS = 5000
+TEST_INTERVAL_EPOCHS = 100000
 SAVE_INTERVAL_EPOCHS = 200
 
-MONITOR_DIR = 'log/monitor/' #the path to save monitor file
-MODEL_DIR = 'log/model' # the path to save deep model
-PARAM_DIR = 'log/param' # the path to save the parameters
-TRA_DIR = 'log/trajectory.csv' # the path to save trajectory
+ACTION_LIST = [
+    (30,  0, 0), # forward
+    (20, 15, 0),
+    (20,-15, 0),
+    (10, 30, 0),
+    (10,-30, 0),
+    (0 ,  0, 1),
+]#velocity  angle trigger
 
+LOG_NAME_SAVE = 'log-door-test'
+MONITOR_DIR = LOG_NAME_SAVE + '/monitor/' #the path to save monitor file
+MODEL_DIR = LOG_NAME_SAVE + '/model' # the path to save deep model
+PARAM_DIR = LOG_NAME_SAVE + '/param' # the path to save the parameters
+TRA_DIR = LOG_NAME_SAVE + '/trajectory.csv' # the path to save trajectory
+
+LOG_NAME_READ = 'log-door-v0'
 #the path to reload weights, monitor and params
-weights_path = 'log/model/dqn_ep' + str(RESTART_EP)+ '.h5'
-monitor_path = 'log/monitor/'+ str(RESTART_EP)
-params_json = 'log/param/dqn_ep' + str(RESTART_EP) + '.json'
+weights_path = LOG_NAME_READ + '/model/dqn_ep' + str(RESTART_EP)+ '.h5'
+monitor_path = LOG_NAME_READ + '/monitor/'+ str(RESTART_EP)
+params_json = LOG_NAME_READ + '/param/dqn_ep' + str(RESTART_EP) + '.json'
+
