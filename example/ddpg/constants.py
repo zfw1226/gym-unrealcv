bd31baa02e0fc0ab62fe334f322d8852166bd168
zfw <zfw1226@gmail.com>
2017-07-03 17:11:20 +0800

    new action definition
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/ddpg/constants.py
+++ example/ddpg/constants.py
@@ -1,4 +1,4 @@
-ENV_NAME = 'Search-RrPlant-v1'
+ENV_NAME = 'Search-Arch1Test-v0'
 
 CONTINUE = False
 RESTART_EP = 8000
@@ -13,8 +13,6 @@ LEARN_START_STEP = 5000
 INPUT_SIZE = 150 # pre 100
 INPUT_CHANNELS = 3
 BATCH_SIZE = 64
-VELOCITY_MAX = 100
-ANGLE_MAX = 60 # +-30
 
 LEARNINGRATE_CRITIC  = 0.001
 LEARNINGRATE_ACTOR = 0.0001
@@ -28,7 +26,7 @@ MAX_EXPLORE_STEPS = 10000
 TEST_INTERVAL_EPOCHS = 1000
 SAVE_INTERVAL_EPOCHS = 200
 
-LOG_NAME_SAVE = 'log-baseline-v2.2'
+LOG_NAME_SAVE = 'log-test'
 MONITOR_DIR = LOG_NAME_SAVE + '/monitor/' #the path to save monitor file
 MODEL_DIR = LOG_NAME_SAVE + '/model' # the path to save deep model
 PARAM_DIR = LOG_NAME_SAVE + '/param' # the path to save the parameters
