0005a9a222e197b42388866ed411d84fc6ff2036
zfw <zfw1226@gmail.com>
2017-07-06 23:54:36 +0800

    delete auto download binary, update IO
    Signed-off-by: zfw <zfw1226@gmail.com>

--- gym_unrealcv/envs/run_docker.py
+++ gym_unrealcv/envs/run_docker.py
@@ -11,53 +11,43 @@ ENV = dict(
 )
 
 class RunDocker():
-    def __init__(self, IMAGE = 'zfw1226/unreal-gpu:v0.1',):
+    def __init__(self, path2env, IMAGE = 'zfw1226/unreal-gpu:v0.1',):
        self.docker_client = docker.from_env()
        self.check_image(target_images = IMAGE)
        os.system('xhost +')
        self.image = IMAGE
+       self.path2env = path2env
 
     def start(self,
-              ENV_NAME = 'RealisticRendering',
+              ENV_BIN = '/RealisticRendering_RL/RealisticRendering/Binaries/Linux/RealisticRendering',
               ENV_DIR_DOCKER='/unreal',
-              ENV_DIR_HOST = 'UnrealEnv'):
+              ):
 
-        HOST_DIR = self.get_abspath(ENV_DIR_HOST)
-        binary = HOST_DIR + ENV[ENV_NAME]
-        print binary
-        if not os.path.exists(binary):
-            sh_dir = HOST_DIR + '/' + ENV_NAME + '.sh'
-            print sh_dir
-            if os.path.exists(sh_dir):
-                os.system('sh ' + sh_dir)
-            else :
-                print 'Did not find unreal environment, Please move your binary file to env/UnrealEnv'
-                sys.exit()
+        path2binary = os.path.join(self.path2env,ENV_BIN)
+        if not os.path.exists(path2binary):
+            print 'Did not find unreal environment, Please move your binary file to env/UnrealEnv'
+            sys.exit()
 
         docker_cmd = 'nvidia-docker run  -d -it  --env="DISPLAY=:0.0"     --env="QT_X11_NO_MITSHM=1"   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"     --volume="{ENV_DIR_HOST}:{ENV_DIR_DOCKER}:rw"    {IMAGE} '
-        run_cmd = 'bash -c "chown unrealcv {ENV_DIR_DOCKER} -R && su unrealcv -c {ENV_DIR_DOCKER}{ENV_DIR_BIN}"'
-        cmd = docker_cmd.format(ENV_DIR_HOST=HOST_DIR, ENV_DIR_DOCKER=ENV_DIR_DOCKER, IMAGE=self.image) + run_cmd.format(
-            ENV_DIR_DOCKER=ENV_DIR_DOCKER, ENV_DIR_BIN=ENV[ENV_NAME])
+        run_cmd = 'bash -c "chown unrealcv {ENV_DIR_DOCKER} -R && su unrealcv -c {ENV_DIR_BIN_DOCKER}"'
+        cmd = docker_cmd.format(ENV_DIR_HOST=self.path2env, ENV_DIR_DOCKER=ENV_DIR_DOCKER, IMAGE=self.image) + \
+              run_cmd.format(ENV_DIR_DOCKER=ENV_DIR_DOCKER, ENV_DIR_BIN_DOCKER = os.path.join(ENV_DIR_DOCKER,ENV_BIN))
+  
         print cmd
         os.system(cmd)
-        time.sleep(10)
         self.container = self.docker_client.containers.list()
-        return self.get_ip(),HOST_DIR
 
+        return self.get_ip()
 
     def get_ip(self):
         return self.container[0].attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
-    def get_abspath(self, relativepath):
-        paths = sys.path
-        for p in paths:
-           if p[-20:].find('gym-unrealcv') > 0:
-               gympath = p
 
-       
-        return os.path.join(gympath,'gym_unrealcv/envs',relativepath)
+    def get_path2UnrealEnv(self):
+        import gym_unrealcv
+        gympath = os.path.dirname(gym_unrealcv.__file__)
+        return os.path.join(gympath,'envs/UnrealEnv')
 
     def close(self):
-        #.container[0].stop()
         self.container[0].remove(force = True)
 
     def check_image(self,target_images='zfw1226/unreal-gpu:v0.1'):
@@ -82,7 +72,7 @@ class RunDocker():
 
 #docker = RunDocker()
 
-#ip,pwd = docker.start('RealisticRendering')
+#ip,pwd = docker.start('ArchinteriorsVol2Sceen1')
 #print ip
 #time.sleep(50)
 #docker.close()
