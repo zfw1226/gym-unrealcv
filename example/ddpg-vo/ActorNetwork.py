86806043c38c387859be34a3373b3751d151aaf2
zfw <zfw1226@gmail.com>
2017-07-07 15:57:47 +0800

    subtask: estimate vo
    Signed-off-by: zfw <zfw1226@gmail.com>

---
+++ example/ddpg-vo/ActorNetwork.py
@@ -0,0 +1,65 @@
+import numpy as np
+import math
+from keras.initializations import normal, identity
+from keras.models import model_from_json
+from keras.models import Sequential, Model
+#from keras.engine.training import collect_trainable_weights
+from keras.layers import Dense, Flatten, Input, merge, Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
+import tensorflow as tf
+import keras.backend as K
+
+
+class ActorNetwork(object):
+    def __init__(self, sess, state_shape, action_size, TAU, LEARNING_RATE):
+        self.sess = sess
+        self.TAU = TAU
+        self.LEARNING_RATE = LEARNING_RATE
+
+        K.set_session(sess)
+
+        #Now create the model
+        self.model , self.weights, self.state = self.create_actor_network(state_shape, action_size)
+        self.target_model, self.target_weights, self.target_state = self.create_actor_network(state_shape, action_size)
+        self.action_gradient = tf.placeholder(tf.float32,[None, action_size])
+        self.params_grad = tf.gradients(self.model.output, self.weights, -self.action_gradient)
+        grads = zip(self.params_grad, self.weights)
+        self.optimize = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(grads)
+        self.sess.run(tf.global_variables_initializer())
+
+    def train(self, states, action_grads):
+        self.sess.run(self.optimize, feed_dict={
+            self.state: states,
+            self.action_gradient: action_grads
+        })
+
+    def target_train(self):
+        actor_weights = self.model.get_weights()
+        actor_target_weights = self.target_model.get_weights()
+        for i in xrange(len(actor_weights)):
+            actor_target_weights[i] = self.TAU * actor_weights[i] + (1 - self.TAU)* actor_target_weights[i]
+        self.target_model.set_weights(actor_target_weights)
+
+    def create_actor_network(self, state_shape,action_size):
+        print("Now we build the model")
+
+        S = Input(shape= state_shape)
+
+        c1 = Convolution2D(32, 3, 3, activation='relu')(S)
+        c2 = Convolution2D(32, 3, 3, activation='relu')(c1)
+        c3 = MaxPooling2D(pool_size=(2, 2))(c2)
+
+        c4 = Convolution2D(32, 3, 3, activation='relu')(c3)
+        c5 = Convolution2D(32, 3, 3, activation='relu')(c4)
+        c6 = MaxPooling2D(pool_size=(2, 2))(c5)
+        c7 = Flatten()(c6)
+        h1 = Dense(512, activation='relu')(c7)
+        h2 = Dense(256, activation='relu')(h1)
+        action = Dense(action_size, activation = 'sigmoid')(h2)
+
+
+        model = Model(input=S, output=action)
+        model.summary()
+
+        return model, model.trainable_weights, S
+
---
+++ example/ddpg-vo/ddpg.py
@@ -0,0 +1,152 @@
+import time
+from distutils.dir_util import copy_tree
+import os
+import json
+import random
+import numpy as np
+
+import tensorflow as tf
+import memory
+import keras.backend as K
+from ActorNetwork import ActorNetwork
+from CriticNetwork import CriticNetwork
+from keras.callbacks import CSVLogger
+
+class DDPG:
+    def __init__(self, outputs, memorySize, discountFactor,
+                 learningRate_Critic, learningRate_Actor, target_update_rate,
+                 img_rows, img_cols, img_channels):
+        """
+        Parameters:
+            - outputs: output size
+            - memorySize: size of the memory that will store each state
+            - discountFactor: the discount factor (gamma)
+            - learningRate: learning rate
+            - learnStart: steps to happen before for learning. Set to 128
+        """
+        self.action_size = outputs
+        self.memory = memory.Memory(memorySize)
+        self.discountFactor = discountFactor
+        self.learningRateCritic = learningRate_Critic
+        self.learningRateActor = learningRate_Actor
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.img_channels = img_channels
+        self.target_update_rate = target_update_rate
+
+        self.img_shape = (self.img_channels, self.img_rows, self.img_cols)
+        if K.image_dim_ordering() == 'tf':
+            self.img_shape = (self.img_rows, self.img_cols, self.img_channels)
+
+
+        with tf.device('/gpu:0'):
+            config = tf.ConfigProto()
+            config.gpu_options.allow_growth = True
+            self.sess = tf.Session(config=config)
+            K.set_session(self.sess)
+
+        print 'tf config!'
+
+        self.actor = ActorNetwork(self.sess, self.img_shape, self.action_size,  self.target_update_rate, self.learningRateActor)
+        print 'actor'
+        self.critic = CriticNetwork(self.sess, self.img_shape, self.action_size,  self.target_update_rate, self.learningRateCritic)
+        print 'critic'
+        self.csv_logger = CSVLogger('critic1.log',append = True)
+
+
+    def updateTargetNetwork(self,model,target,updaterate):
+        weightMatrix = []
+        for layer in model.layers:
+            weights = layer.get_weights()
+            weightMatrix.append(weights)
+        i = 0
+        for layer in target.layers:
+            weights_pre = layer.get_weights()
+            weights = updaterate * weightMatrix[i] + (1 - updaterate) * weights_pre
+            layer.set_weights(weights)
+            i += 1
+
+
+    # select the action with the highest Q value
+    def Action_Noise(self, action_pred, explorationRate):
+
+        #theta = np.array([0.6,1,1])
+        #sigma = np.array([0.3,0.1,0.05])
+        #noise = explorationRate * self.OU_noise(action_pred,  0.0 , theta = 0.6, sigma = 0.3)
+
+        #action = action_pred + noise
+        action = action_pred
+        for i in range(len(action[0])):
+            noise = np.random.normal(0.5,0.5)
+            action[0][i] = (1 - explorationRate) * action_pred[0][i] + explorationRate * noise
+            action[0][i] = max(0,action[0][i])
+            action[0][i] = min(1,action[0][i])
+
+
+        return action[0]
+
+    def addMemory(self, state, action, reward, newState, isFinal):
+        self.memory.addMemory(state, action, reward, newState, isFinal)
+
+    def saveMemory(self,state,  action, reward, newState, isFinal ):
+        with open('/home/UAV/dqn_data/memory.npy', 'a') as f:
+            #f.writelines(str(state)+'\n')
+            np.save(f, state)
+
+    def getMemorySize(self):
+        return self.memory.getCurrentSize()
+
+
+    def learnOnMiniBatch(self, miniBatchSize):
+        state_batch, action_batch, reward_batch, newState_batch, isFinal_batch, motion_batch \
+            = self.memory.getMiniBatch(miniBatchSize)
+
+        isFinal_batch = np.array(isFinal_batch) + 0
+
+        actor_output = self.actor.target_model.predict_on_batch(np.array(newState_batch))
+
+        critic_input = [np.array(newState_batch),np.array(actor_output)]
+        qValuesNewState_batch, premotion_batch = self.critic.target_model.predict_on_batch(critic_input)
+
+        Y_value = np.zeros([miniBatchSize, self.action_size])
+        for k in range(miniBatchSize):
+            Y_value[k] = reward_batch[k] + (1 - isFinal_batch[k]) * self.discountFactor * qValuesNewState_batch[k]
+
+
+        action_batch = np.array(action_batch)
+
+        state_batch = np.array(state_batch)
+        X_input = [state_batch, action_batch]
+        motion_batch = np.array(motion_batch)
+        self.critic.model.fit(X_input, [Y_value,np.array(motion_batch)], validation_split=0.0,
+                              batch_size=miniBatchSize, nb_epoch=1, verbose = 0, callbacks=[self.csv_logger])
+
+        a_for_grad = self.actor.model.predict(state_batch)
+        grads = self.critic.gradients(state_batch, a_for_grad)
+
+        self.actor.train(state_batch, grads)
+
+        self.actor.target_train()
+        self.critic.target_train()
+        #self.updateTargetNetwork(self.critic.model, self.critic.target_model, self.target_update_rate)
+        #self.updateTargetNetwork(self.actor.model, self.actor.target_model, self.target_update_rate)
+
+    def saveModel(self, path):
+        self.critic.model.save_weights(path+'Critic_model.h5')
+        self.actor.model.save_weights(path+'Actor_model.h5')
+
+    def loadWeights(self, critic_weight_path, actor_weight_path):
+        self.critic.model.load_weights(critic_weight_path)
+        self.critic.target_model.load_weights(critic_weight_path)
+        self.actor.model.load_weights(actor_weight_path)
+        self.actor.target_model.load_weights(actor_weight_path)
+
+    def OU_noise(self,x, mu, theta, sigma):
+        return theta * (mu - x) + sigma * np.random.randn(1)
+
+    def random_noise(self):
+        return np.random.normal(0,0.5)
+
+
+
+
