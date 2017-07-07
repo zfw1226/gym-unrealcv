e614a662357730b80250d4d724846841b2ef72d8
zfw <zfw1226@gmail.com>
2017-07-07 16:03:31 +0800

    train arch
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/dqn-linear/constants.py
+++ example/dqn-linear/constants.py
@@ -1,7 +1,7 @@
-ENV_NAME = 'Search-RrPlant-v0'
+ENV_NAME = 'Search-Arch1Door-v0'
 
-CONTINUE = True #load a pre-trained model
-RESTART_EP = 2400 # the episode number of the pre-trained model
+CONTINUE = False #load a pre-trained model
+RESTART_EP = 3400 # the episode number of the pre-trained model
 
 TRAIN = True # train the network
 USE_TARGET_NETWORK = False # use the target network
@@ -10,7 +10,7 @@ MAP = False # show the trajectory in 2d map
 
 MAX_EPOCHS = 100000 # max episode number
 
-MEMORY_SIZE = 10000
+MEMORY_SIZE = 100000
 LEARN_START_STEP = 5000
 INPUT_SIZE = 150
 INPUT_CHANNELS = 3
@@ -19,7 +19,7 @@ LEARNING_RATE = 1e-3  # 1e6
 GAMMA = 0.95
 INITIAL_EPSILON = 1  # starting value of epsilon
 FINAL_EPSILON = 0.1  # final value of epsilon
-MAX_EXPLORE_STEPS = 5000
+MAX_EXPLORE_STEPS = 20000
 TEST_INTERVAL_EPOCHS = 100000
 SAVE_INTERVAL_EPOCHS = 200
 
@@ -32,7 +32,7 @@ ACTION_LIST = [
     (0 ,  0, 1),
 ]#velocity  angle trigger
 
-LOG_NAME_SAVE = 'log-angle-v3.4'
+LOG_NAME_SAVE = 'dqn-angle-v7.2'
 MONITOR_DIR = LOG_NAME_SAVE + '/monitor/' #the path to save monitor file
 MODEL_DIR = LOG_NAME_SAVE + '/model' # the path to save deep model
 PARAM_DIR = LOG_NAME_SAVE + '/param' # the path to save the parameters
@@ -40,7 +40,7 @@ TRA_DIR = LOG_NAME_SAVE + '/trajectory.csv' # the path to save trajectory
 LOSS_DIR = LOG_NAME_SAVE + '/loss.csv'
 
 
-LOG_NAME_READ = 'log-angle-v3.3'
+LOG_NAME_READ = 'dqn-angle-v6.1'
 #the path to reload weights, monitor and params
 weights_path = LOG_NAME_READ + '/model/dqn_ep' + str(RESTART_EP)+ '.h5'
 monitor_path = LOG_NAME_READ + '/monitor/'+ str(RESTART_EP)
