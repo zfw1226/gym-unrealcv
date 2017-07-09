0870e76094aedd8ae3e35130c139104d3ce6dd9d
zfw <zfw1226@gmail.com>
2017-07-09 21:13:22 +0800

    update version
    Signed-off-by: zfw <zfw1226@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_unrealcv',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym','opencv-python',],  # And any other dependencies foo needs
 )  
