89a36e32a7cef956d44a2f9b7b350f4e0510b3ee
zfw <zfw1226@gmail.com>
2017-07-09 21:29:25 +0800

    change default env
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/ddpg/constants.py
+++ example/ddpg/constants.py
@@ -1,18 +1,19 @@
-ENV_NAME = 'Search-Arch1DoorContinuous-v0'
+ENV_NAME = 'Search-RrPlantContinuous-v0'
 
 CONTINUE = False
 RESTART_EP = 8000
 TRAIN = True
-SHOW = True
+SHOW = False
 MAP = False
 
 MAX_EPOCHS = 100000
 MAX_STEPS_PER_EPOCH = 10000
-MEMORY_SIZE = 30000
+MEMORY_SIZE = 10000
 LEARN_START_STEP = 5000
+MAX_EXPLORE_STEPS = 10000
+
 INPUT_SIZE = 150 # pre 100
 BATCH_SIZE = 64
-
 LEARNINGRATE_CRITIC  = 0.001
 LEARNINGRATE_ACTOR = 0.0001
 TARGET_UPDATE_RATE = 0.001
@@ -21,18 +22,18 @@ GAMMA = 0.95
 INITIAL_EPSILON = 1  # starting value of epsilon
 FINAL_EPSILON = 0.1  # final value of epsilon
 
-MAX_EXPLORE_STEPS = 10000
-TEST_INTERVAL_EPOCHS = 1000
-SAVE_INTERVAL_EPOCHS = 200
 
-LOG_NAME_SAVE = 'log-test'
+TEST_INTERVAL_EPOCHS = 1000
+SAVE_INTERVAL_EPOCHS = 500
+
+LOG_NAME_SAVE = 'log'
 MONITOR_DIR = LOG_NAME_SAVE + '/monitor/' #the path to save monitor file
 MODEL_DIR = LOG_NAME_SAVE + '/model' # the path to save deep model
 PARAM_DIR = LOG_NAME_SAVE + '/param' # the path to save the parameters
 TRA_DIR = LOG_NAME_SAVE + '/trajectory.csv' # the path to save trajectory
 
 #the path to reload weights, monitor and params
-LOG_NAME_READ = 'log-angle-v4.0'
+LOG_NAME_READ = 'log'
 critic_weights_path = LOG_NAME_READ + '/model/ep' + str(RESTART_EP) + 'Critic_model.h5'
 actor_weights_path = LOG_NAME_READ + '/model/ep' + str(RESTART_EP) + 'Actor_model.h5'
 monitor_path = LOG_NAME_READ + '/monitor/'+ str(RESTART_EP)
