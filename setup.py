bd684d951eded729c509eb13695d155415eddb32
zfw <zfw1226@gmail.com>
2017-07-10 17:53:30 +0800

    update dependencies
    Signed-off-by: zfw <zfw1226@gmail.com>

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_unrealcv',
       version='0.0.2',
-      install_requires=['gym','opencv-python',],  # And any other dependencies foo needs
+      install_requires=['gym','opencv-python','docker','matplotlib','numpy','unrealcv'],  # And any other dependencies foo needs
 )  
