ba83de1c0589398beb1b7b7118022220461bea79
zfw <zfw1226@gmail.com>
2017-07-10 18:57:32 +0800

    delete simple and search
    Signed-off-by: zfw <zfw1226@gmail.com>

--- gym_unrealcv/__init__.py
+++ gym_unrealcv/__init__.py
@@ -2,15 +2,6 @@ from gym.envs.registration import register
 import logging
 logger = logging.getLogger(__name__)
 docker = False  # True: use nvidia docker   False: do not use nvidia-docker
-register(
-    id='Unrealcv-Simple-v0',
-    entry_point='gym_unrealcv.envs:UnrealCvSimple'
-)
-register(
-    id='Unrealcv-Search-v0',
-    entry_point='gym_unrealcv.envs:UnrealCvSearch'
-)
-
 
 #RrPlant7
 register(
--- gym_unrealcv/envs/__init__.py
+++ gym_unrealcv/envs/__init__.py
@@ -1,3 +1,1 @@
-from gym_unrealcv.envs.unrealcv_simple import UnrealCvSimple
-from gym_unrealcv.envs.unrealcv_search import UnrealCvSearch
 from gym_unrealcv.envs.unrealcv_search_base import UnrealCvSearch_base
