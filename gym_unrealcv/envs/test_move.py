f8c157c9bbce06cd60b14d4e7bf91b7c178dd9ce
zfw1226 <zfw1226@gmail.com>
2017-07-10 15:30:47 +0800

    Delete test_move.py

--- gym_unrealcv/envs/test_move.py
+++
@@ -1,8 +0,0 @@
-from unrealcv_cmd import  UnrealCv
-import time
-unrealcv = UnrealCv()
-#unrealcv.set_position(0,-530,-3720,90)
-unrealcv.set_rotation(0,0,-180,350)
-for i in range(10000):
-    unrealcv.move(0,0,20)
-    time.sleep(0.2)
