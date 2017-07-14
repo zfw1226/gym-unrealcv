c1ef2ff07d5be15c035cb245821847a5e93e3f6d
zfw <zfw1226@gmail.com>
2017-07-14 09:29:26 +0800

    delete docker python api
    Signed-off-by: zfw <zfw1226@gmail.com>

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_unrealcv',
       version='0.0.2',
-      install_requires=['gym','docker','matplotlib','numpy','unrealcv'],  # And any other dependencies foo needs
+      install_requires=['gym','matplotlib','numpy','unrealcv'],  # And any other dependencies foo needs
 )  
