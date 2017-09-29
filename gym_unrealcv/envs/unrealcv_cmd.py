b5c4273cedef0069ff48e28dc88f2857a34e7d59
zfw <zfw1226@gmail.com>
2017-09-29 16:59:36 +0800

    add function to calibrate depth function
    Signed-off-by: zfw <zfw1226@gmail.com>

--- gym_unrealcv/envs/unrealcv_cmd.py
+++ gym_unrealcv/envs/unrealcv_cmd.py
@@ -80,7 +80,7 @@ class UnrealCv:
             pose_str = msg[13:].split()
             self.arm['grip'] = np.array(pose_str, dtype=np.float16)
             self.arm['flag_grip'] = True
-        else:
+        elif message != 'move':
             self.message.append(message)
 
     def read_message(self):
@@ -145,9 +145,20 @@ class UnrealCv:
         res = client.request(cmd.format(cam_id=cam_id))
         import StringIO
         depth = np.load(StringIO.StringIO(res))
-        depth[depth>10.0] = 10.0
+        depth[depth>100.0] = 0
         #self.show_img(depth,'depth')
-        return np.expand_dims(depth,axis=2)
+        #return depth
+        return np.expand_dims(depth,axis=-1)
+
+    def convert2planedepth(self,PointDepth, f=320):
+        H = PointDepth.shape[0]
+        W = PointDepth.shape[1]
+        i_c = np.float(H) / 2 - 1
+        j_c = np.float(W) / 2 - 1
+        columns, rows = np.meshgrid(np.linspace(0, W - 1, num=W), np.linspace(0, H - 1, num=H))
+        DistanceFromCenter = ((rows - i_c) ** 2 + (columns - j_c) ** 2) ** (0.5)
+        PlaneDepth = PointDepth / (1 + (DistanceFromCenter / f) ** 2) ** (0.5)
+        return PlaneDepth
 
     def get_rgbd(self,cam_id):
         rgb = self.read_image(cam_id,'lit')
