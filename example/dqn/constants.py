d666f74602cd8eb3e1f2ca631f06e7463fd7c5a9
zfw <zfw1226@gmail.com>
2017-09-29 17:13:13 +0800

    constant for dqn
    Signed-off-by: zfw <zfw1226@gmail.com>

--- example/dqn/constants.py
+++ example/dqn/constants.py
@@ -1,28 +1,28 @@
-ENV_NAME = 'Search-RrMultiSocketsDiscrete-v0'
+ENV_NAME = 'RobotArm-Discrete-v0'
 
 CONTINUE = False #load a pre-trained model
 RESTART_EP = 6000 # the episode number of the pre-trained model
 
 TRAIN = True # train the network
-USE_TARGET_NETWORK = True # use the target network
-SHOW = True # show the current state, reward and action
+USE_TARGET_NETWORK = False # use the target network
+SHOW = False # show the current state, reward and action
 MAP = False # show the trajectory in 2d map
 
 TF_DEVICE = '/gpu:0'
 MAX_EPOCHS = 10000 # max episode number
-MEMORY_SIZE = 30000
-LEARN_START_STEP = 5000
-INPUT_SIZE = 100
-BATCH_SIZE = 64
+MEMORY_SIZE = 50000
+LEARN_START_STEP = 10000
+INPUT_SIZE = 4
+BATCH_SIZE = 32
 LEARNING_RATE = 1e-3  # 1e6
 GAMMA = 0.95
 INITIAL_EPSILON = 1  # starting value of epsilon
 FINAL_EPSILON = 0.1  # final value of epsilon
-MAX_EXPLORE_STEPS = 15000
+MAX_EXPLORE_STEPS = 25000
 TEST_INTERVAL_EPOCHS = 100000
-SAVE_INTERVAL_EPOCHS = 500
+SAVE_INTERVAL_EPOCHS = 600
 
-LOG_NAME_SAVE = 'log-sockets'
+LOG_NAME_SAVE = 'log'
 MONITOR_DIR = LOG_NAME_SAVE + '/monitor/' #the path to save monitor file
 MODEL_DIR = LOG_NAME_SAVE + '/model' # the path to save deep model
 PARAM_DIR = LOG_NAME_SAVE + '/param' # the path to save the parameters
@@ -31,6 +31,6 @@ TRA_DIR = LOG_NAME_SAVE + '/trajectory.csv' # the path to save trajectory
 LOG_NAME_READ = 'log'
 #the path to reload weights, monitor and params
 weights_path = LOG_NAME_READ + '/model/dqn_ep' + str(RESTART_EP)+ '.h5'
-monitor_path = LOG_NAME_READ + '/monitor/'+ str(RESTART_EP)
+monitor_path = LOG_NAME_READ + '/monit` or/'+ str(RESTART_EP)
 params_json = LOG_NAME_READ + '/param/dqn_ep' + str(RESTART_EP) + '.json'
 
