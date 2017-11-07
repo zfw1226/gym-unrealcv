2d0b6cff5a301bf7796b4c0422761ad999f3e2a4
zfw1226@gmail.com <k15325025312>
2017-11-07 12:00:42 +0800

    random layout

--- gym_unrealcv/envs/unrealcv_tracking.py
+++ gym_unrealcv/envs/unrealcv_tracking.py
@@ -28,6 +28,8 @@ Recommend object list in Arch1
 'BP_door_001_C_0','BP_door_002_C_0'
 '''
 
+
+
 class UnrealCvTracking_base(gym.Env):
    def __init__(self,
                 setting_file = 'search_quadcopter.json',
@@ -35,7 +37,7 @@ class UnrealCvTracking_base(gym.Env):
                 test = True,               # if True will use the test_xy as start point
                 action_type = 'discrete',  # 'discrete', 'continuous'
                 observation_type = 'rgbd', # 'color', 'depth', 'rgbd'
-                reward_type = 'bbox', # distance
+                reward_type = 'distance', # distance
                 docker = False,
                 ):
 
@@ -106,6 +108,15 @@ class UnrealCvTracking_base(gym.Env):
 
      self.rendering = False
 
+     self.objects_env = ['road_A_55', 'road_B_58', 'road_B2_61', 'road_B3_67', 'road_B4_70', 'road_B5', 'flg_tree_01_74', 'flg_tree_02_80', 'flg_tree_03_83', 'flg_tree_89', 'flg_tree_92', 'flg_tree_95', 'flg_tree_98', 'flg_tree_101', 'flg_tree_104', 'flg_tree_107', 'flg_bush_02_114', 'flg_bush_01_117', 'flg_bush_120', 'flg_fern_123', 'flg_fern2_127', 'prp_trafficLight_27', 'flg_bush_31', 'flg_bush_34', 'flg_fern3', 'flg_fern4', 'flg_tree_39', 'flg_tree_10', 'flg_tree_11', 'gardenPlanter_A_51', 'gardenPlanter_B_55', 'flg_tree_58', 'flg_bush_64', 'flg_bush_67', 'flg_bush_70', 'flg_tree_76', 'flg_tree_79', 'gardenPlanter_A2', 'flg_tree_15', 'flg_tree_16', 'flg_tree_17', 'flg_tree_18', 'flg_tree_19', 'flg_groundLeaves_91', 'StoreSign_4_mdl_94', 'StoreSign_2_mdl_97', 'Sign_4_mdl_128', 'StoreSign_1_mdl_131', 'StoreSign_3_mdl_137', 'Building_9_6', 'Building_7_17', 'Building_6_20', 'Building_9_32', 'Building_6_38', 'building_1_01_47', 'Building_2_01_50', 'Building_3_53', 'Building_6_56', 'Building_4_02_59', 'Building_8_01_74', 'Building_4_01_77', 'building_1_86', 'prp_garbageCan_5', 'prp_bench_14', 'prp_bench2', 'prp_bench3', 'prp_bench4', 'Floor2', 'prp_tableUmbrella3', 'prp_chair5', 'prp_table3', 'prp_chair6', 'prp_tableUmbrella4', 'prp_chair7', 'prp_table4', 'prp_chair8', 'prp_tableUmbrella5', 'prp_chair9', 'prp_table5', 'prp_chair10', 'prp_tableUmbrella6', 'prp_chair11', 'prp_table6', 'prp_chair12', 'prp_tableUmbrella7', 'prp_chair13', 'prp_table7', 'prp_chair14', 'prp_tableUmbrella8', 'prp_chair15', 'prp_table8', 'prp_chair16', 'prp_tableUmbrella9', 'prp_chair17', 'prp_table9', 'prp_chair18', 'prp_tableUmbrella10', 'prp_chair19', 'prp_table10', 'prp_chair20', 'prp_tableUmbrella11', 'prp_chair21', 'prp_table11', 'prp_chair22', 'prp_tableUmbrella12', 'prp_chair23', 'prp_table12', 'prp_chair24', 'prp_tableUmbrella13', 'prp_chair25', 'prp_table13', 'prp_chair26']
+
+     if 'random' in self.reset_type:
+         self.show_list = self.objects_env
+         self.hiden_list = random.sample(self.objects_env, 15)
+         for x in self.hiden_list:
+            self.show_list.remove(x)
+
+
    def _render(self, mode='human', close=False):
        self.rendering = True
 
@@ -128,21 +139,23 @@ class UnrealCvTracking_base(gym.Env):
 
         if self.action_type == 'discrete':
             #keyboard
+            '''
             if self.discrete_actions[action] != "Stop":
                 self.unrealcv.keyboard(self.discrete_actions[action], duration=0.1)
-
+            '''
             # linear
-            #(velocity, angle) = self.discrete_actions[action]
+            (velocity, angle) = self.discrete_actions[action]
         else:
             (velocity, angle) = action
-        #info['Collision'] = self.unrealcv.move(self.cam_id, angle, velocity)
+
+        info['Collision'] = self.unrealcv.move(self.cam_id, angle, velocity)
 
         self.count_steps += 1
 
-        info['Pose'] = self.unrealcv.get_pose('hard')
+        info['Pose'] = self.unrealcv.get_pose()
         #print info['Pose']
         self.target_pos = self.unrealcv.get_object_pos(self.target_list[0])
-        print self.target_pos
+        #print self.target_pos
         info['Direction'] = self.get_direction(self.target_pos, info['Pose'][:3]) - info['Pose'][-1]
         if info['Direction'] < -180:
             info['Direction'] += 360
@@ -191,6 +204,26 @@ class UnrealCvTracking_base(gym.Env):
 
         return state, info['Reward'], info['Done'], info
    def _reset(self, ):
+       if 'random' in self.reset_type:
+           num_update = 3
+           to_hiden = random.sample(self.show_list,num_update)
+           #print to_hiden
+           for x in to_hiden:
+               self.show_list.remove(x)
+               self.hiden_list.append(x)
+           self.unrealcv.hide_objects(to_hiden)
+
+           to_show = random.sample(self.hiden_list[:-num_update],num_update)
+           #print to_show
+           for x in to_show:
+               self.hiden_list.remove(x)
+               self.show_list.append(x)
+           self.unrealcv.show_objects(to_show)
+
+
+
+
+
 
        self.target_pos = self.unrealcv.get_object_pos(self.target_list[0])
        time.sleep(0.5 + 0.3*random.random())
