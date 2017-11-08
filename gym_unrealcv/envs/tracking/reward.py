3ed4681a93f261d03a537a4be0f50aee544b8751
zfw1226@gmail.com <k15325025312>
2017-11-08 16:10:04 +0800

    reward function

--- gym_unrealcv/envs/tracking/reward.py
+++ gym_unrealcv/envs/tracking/reward.py
@@ -7,17 +7,20 @@ class Reward():
     def __init__(self, setting):
 
         self.dis2target_last = 0
-        self.dis_exp = 150
+        self.dis_exp = setting['exp_distance']
 
 
     def reward_distance(self, dis2target_now, direction_error):
         #reward = (100.0 / max(dis2target_now,100)) * np.cos(direction_error/360.0*np.pi)
         direction_error  = direction_error/180.0*np.pi
-        cos_factor = np.cos(direction_error)
 
-        reward = 1 - abs(self.dis_exp-dis2target_now) / self.dis_exp * (1 - cos_factor) - abs(direction_error/np.pi)
+        cos_factor = np.cos(direction_error)
+        e_dis = self.dis_exp-dis2target_now*cos_factor
+        e_dis_relative = e_dis / self.dis_exp
+
+        reward = 1 - abs(e_dis_relative) - abs(direction_error/(np.pi/4))
         self.dis2target_last = dis2target_now
-        #print reward,direction_error
+        #print reward,direction_error,dis2target_now
         return reward
 
     def reward_move(self):
