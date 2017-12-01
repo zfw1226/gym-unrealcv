9a25d234452726bd49e6f4993540171f7cf1a289
zfw1226@gmail.com <k15325025312>
2017-12-01 22:58:52 +0800

    update env name

--- RealisticRendering.sh
+++ RealisticRendering.sh
@@ -1,4 +1,4 @@
 wget https://s3-us-west-1.amazonaws.com/unreal-rl/RealisticRendering_RL_3.10.zip
-unzip RealisticRendering_RL.zip
-rm RealisticRendering_RL.zip
-mv RealisticRendering_RL gym_unrealcv/envs/UnrealEnv
+unzip RealisticRendering_RL_3.10.zip
+rm RealisticRendering_RL_3.10.zip
+mv RealisticRendering_RL_3.10 gym_unrealcv/envs/UnrealEnv
