9e945ed94df59ba86e3eb0af58abf718138d3ba1
zfw <zfw1226@gmail.com>
2018-05-16 16:11:01 +0800

    make exp_dis changeable when running

--- gym_unrealcv/envs/tracking/reward.py
+++ gym_unrealcv/envs/tracking/reward.py
@@ -9,18 +9,16 @@ class Reward():
         self.dis2target_last = 0
         self.dis_exp = setting['exp_distance']
 
-
-    def reward_distance(self, dis2target_now, direction_error):
-        #reward = (100.0 / max(dis2target_now,100)) * np.cos(direction_error/360.0*np.pi)
+    def reward_distance(self, dis2target_now, direction_error, dis_exp=None):
+        #  reward = (100.0 / max(dis2target_now,100)) * np.cos(direction_error/360.0*np.pi)
+        if dis_exp is None:
+            dis_exp = self.dis_exp
         direction_error = direction_error/180.0*np.pi
 
-        cos_factor = np.cos(direction_error)
-        e_dis = self.dis_exp-dis2target_now * cos_factor
-        e_dis_relative = e_dis / self.dis_exp
+        e_dis = dis_exp-dis2target_now * np.cos(direction_error)
+        #  e_dis_relative = e_dis / self.dis_exp
+        e_dis_relative = e_dis / dis_exp
         reward = 1 - min(abs(e_dis_relative), 2) - min(abs(direction_error/(np.pi/4)), 2)
         self.dis2target_last = dis2target_now
         reward = max(reward, -1)
         return reward
-
-    def reward_move(self):
-        return 10
