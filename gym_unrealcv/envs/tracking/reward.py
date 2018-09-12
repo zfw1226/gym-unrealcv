603d242f97f52fc0a864273941b601632b0b7e49
zfw <zfw1226@gmail.com>
2018-09-12 21:27:16 +0800

    modify rewards

--- gym_unrealcv/envs/tracking/reward.py
+++ gym_unrealcv/envs/tracking/reward.py
@@ -28,11 +28,8 @@ class Reward():
         if dis_exp is None:
             dis_exp = self.dis_exp
         direction_error = abs(abs(direction_error) - 45)/45.0
-
-        e_dis = abs(abs(dis_exp - dis2target_now) - dis_exp)
-        #  e_dis_relative = e_dis / self.dis_exp
-        e_dis_relative = e_dis / dis_exp
+        e_dis = abs(abs(dis_exp - dis2target_now) - dis_exp) / dis_exp
         # reward = 1 - 2 * min(abs(e_dis_relative), 1) + min(abs(direction_error/(np.pi/4)), 2)
-        reward = 1 - min(abs(e_dis_relative), 1) - min(direction_error, 1)
+        reward = 1 - min(e_dis, 1) - min(direction_error, 1)
         self.dis2target_last = dis2target_now
         return reward
