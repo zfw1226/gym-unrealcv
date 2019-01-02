ed762613dd8dc0d7218118e2197a07110a99498a
zfw <zfw1226@gmail.com>
2019-01-02 14:44:22 +0800

    fix reward

--- gym_unrealcv/envs/tracking/reward.py
+++ gym_unrealcv/envs/tracking/reward.py
@@ -18,8 +18,8 @@ class Reward():
         e_dis = abs(dis_exp - dis2target_now)
         #  e_dis_relative = e_dis / self.dis_exp
         e_dis_relative = e_dis / dis_exp
-        reward = 1 - min(e_dis_relative, 1) - min(direction_error, 1)
-        # reward = 1 - direction_error - e_dis_relative
+        # reward = 1 - min(e_dis_relative, 1) - min(direction_error, 1)
+        reward = 1 - direction_error - e_dis_relative
         self.dis2target_last = dis2target_now
         reward = max(reward, -1)
         return reward
@@ -31,8 +31,8 @@ class Reward():
         direction_error = abs(abs(direction_error) - 45)/45.0
         e_dis = abs(abs(dis_exp - dis2target_now) - dis_exp) / dis_exp
         # reward = 1 - 2 * min(abs(e_dis_relative), 1) + min(abs(direction_error/(np.pi/4)), 2)
-        reward = 1 - min(e_dis, 1) - min(direction_error, 1)
-        # reward = 1 - e_dis - direction_error
+        # reward = 1 - min(e_dis, 1) - min(direction_error, 1)
+        reward = 1 - e_dis - direction_error
         reward = max(reward, -1)
         self.dis2target_last = dis2target_now
         return reward
