ca9474857d05ea1d9debd75437356150d756ac5b
zfw <zfw1226@gmail.com>
2019-07-01 17:02:55 +0800

    delete

--- Arch1.sh
+++
@@ -1,4 +0,0 @@
-wget https://s3-us-west-1.amazonaws.com/unreal-rl/ArchinteriorsVol2Scene1.zip
-unzip ArchinteriorsVol2Scene1.zip
-rm ArchinteriorsVol2Scene1.zip
-mv ArchinteriorsVol2Scene1 gym_unrealcv/envs/UnrealEnv
