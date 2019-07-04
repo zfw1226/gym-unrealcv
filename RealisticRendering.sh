4b319b1eb62f04a33d16c1c847c58ea0d5281b73
zfw <zfw1226@gmail.com>
2019-07-04 22:57:07 +0800

    clean useless env

--- RealisticRendering.sh
+++
@@ -1,4 +0,0 @@
-wget https://s3-us-west-1.amazonaws.com/unreal-rl/RealisticRendering_RL_3.10.zip
-unzip RealisticRendering_RL_3.10.zip
-rm RealisticRendering_RL_3.10.zip
-mv RealisticRendering_RL_3.10 gym_unrealcv/envs/UnrealEnv
