ad6292d49139a80cc7ca9c2e1691bd46c7b78ffe
zfw1226 <zfw1226@gmail.com>
2023-09-05 18:10:27 +0800

    add update_steps

--- gym_unrealcv/envs/wrappers/time_dilation.py
+++ gym_unrealcv/envs/wrappers/time_dilation.py
@@ -3,22 +3,22 @@ from gym import Wrapper
 import time
 
 class TimeDilationWrapper(Wrapper):
-    def __init__(self, env, reference_fps=10):
+    def __init__(self, env, reference_fps=10, update_steps=10):
         super().__init__(env)
-        self.dilation_factor = 1 # the factor by which to slow down the environment
-        self.reference_fps = 10 # the fps at which the environment should run
-
+        self.dilation_factor = 1  # the factor by which to slow down the environment
+        self.reference_fps = reference_fps  # the fps at which the environment should run
+        self.update_steps = update_steps  # the number of steps after which to update the time dilation
     def step(self, action):
-        obs, reward, done, info = self.env.step(action) # take a step in the wrapped environment
+        obs, reward, done, info = self.env.step(action)  # take a step in the wrapped environment
         self.count_steps += 1
-        if self.count_steps % 10 == 0: # update the time dilation every 10 steps
+        if self.count_steps % self.update_steps == 0:  # update the time dilation every 10 steps
             fps = self.count_steps / (time.time() - self.start_time)
             dilation_factor_new = fps / self.reference_fps  # reference fps: 10
             if  dilation_factor_new / self.dilation_factor > 1.1 or dilation_factor_new / self.dilation_factor < 0.9:
                 self.dilation_factor = dilation_factor_new
                 env = self.env.unwrapped
                 env.unrealcv.set_global_time_dilation(self.dilation_factor)
-        return obs, reward, done, info # return the same results as the wrapped environment
+        return obs, reward, done, info  # return the same results as the wrapped environment
 
     def reset(self, **kwargs):
         states = self.env.reset(**kwargs)
